4ea7acdbcfe4cd8f67f1bf6754b17098d3c58976
helio-mesquita <heliomesquita.geo@gmail.com>
2021-10-28 15:17:03 -0300

    Tratamento de exceção para método findById, retornando 404 not found para casos de busca por id inexistente.

--- Server/src/main/java/com/heliomesquita/dev/server/services/UserService.java
+++ Server/src/main/java/com/heliomesquita/dev/server/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.heliomesquita.dev.server.entitites.User;
 import com.heliomesquita.dev.server.repositories.UserRepository;
+import com.heliomesquita.dev.server.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional <User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert (User obj) {
---
+++ Server/src/main/java/com/heliomesquita/dev/server/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.heliomesquita.dev.server.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException (Object id) {
+		super("Resource not found. Id " + id);
+	}
+
+}
